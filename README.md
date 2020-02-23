8eedf8b7965d377d1cac3434304dae822e370139
grenter <1819847843@qq.com>
2020-02-23 17:51:57 +0800

    edit bug

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# py3
+
+环境：python3
+使用方法：
+1、设置路径UC_PATH，MP3_PATH
+2、运行transform.py
--- transform.py
+++ transform.py
@@ -4,16 +4,16 @@ import os
 import re
 import requests
 
-uc_path = 'C:/Users/Grenter/Desktop/transform/Cache/'  # 缓存路径 例 D:/CloudMusic/Cache/
-mp3_path = 'C:/Users/Grenter/Desktop/transform/'  # 存放歌曲路径
+UC_PATH = './Cache/'  # 缓存路径 例 D:/CloudMusic/Cache/
+MP3_PATH = './'  # 存放歌曲路径
 
-class Transform(object):
+class Transform():
     def do_transform(self):
-        files = os.listdir(uc_path)
+        files = os.listdir(UC_PATH)
         for file in files:
             if file[-2:] == 'uc':  # 后缀uc结尾为歌曲缓存
                 print(file)
-                uc_file = open(uc_path + file, mode='rb')
+                uc_file = open(UC_PATH + file, mode='rb')
                 uc_content = uc_file.read()
                 mp3_content = bytearray()
                 for byte in uc_content:
@@ -21,7 +21,7 @@ class Transform(object):
                     mp3_content.append(byte)
                 song_id = self.get_songid_by_filename(file)
                 song_name, singer_name = self.get_song_info(song_id)
-                mp3_file_name = mp3_path + '%s - %s.mp3' % (singer_name, song_name)
+                mp3_file_name = MP3_PATH + '%s - %s.mp3' % (singer_name, song_name)
                 mp3_file = open(mp3_file_name, 'wb')
                 mp3_file.write(mp3_content)
                 uc_file.close()
@@ -36,33 +36,33 @@ class Transform(object):
 
     def get_song_info(self, song_id):
         if not song_id:
-            return '', str(song_id)
+            return str(song_id), ''
 
         try:
-            url = 'https://api.imjad.cn/cloudmusic/'  # 请求url：https://api.imjad.cn/cloudmusic/?type=detail&id=1347203552
+            url = 'https://api.imjad.cn/cloudmusic/'  # 请求url例子：https://api.imjad.cn/cloudmusic/?type=detail&id=1347203552
             payload = {'type': 'detail', 'id': song_id}
             reqs = requests.get(url, params=payload)
             jsons = reqs.json()
-            song_name = jsons['songs'][0]['al']['name']
-            singer_name = jsons['songs'][0]['name']
-            return singer_name, song_name
+            song_name = jsons['songs'][0]['name']
+            singer = jsons['songs'][0]['ar'][0]['name']
+            return song_name, singer
         except:
-            return '', str(song_id)
+            return str(song_id), ''
 
 def check_path():
-    global uc_path, mp3_path
+    global UC_PATH, MP3_PATH
 
-    if not os.path.exists(uc_path):
-        print('缓存路径错误: %s' % uc_path)
+    if not os.path.exists(UC_PATH):
+        print('缓存路径错误: %s' % UC_PATH)
         return False
-    if not os.path.exists(mp3_path):
-        print('目标路径错误: %s' % mp3_path)
+    if not os.path.exists(MP3_PATH):
+        print('目标路径错误: %s' % MP3_PATH)
         return False
 
-    if uc_path[-1] != '/':  # 容错处理 防止绝对路径结尾不是/
-        uc_path += '/'
-    if mp3_path[-1] != '/':
-        mp3_path += '/'
+    if UC_PATH[-1] != '/':  # 容错处理 防止绝对路径结尾不是/
+        UC_PATH += '/'
+    if MP3_PATH[-1] != '/':
+        MP3_PATH += '/'
     return True
 
 if __name__ == '__main__':
